204d3d23ec375df42ef69e52247e353782a5f859
Nishan <nishan.bhattrai@aitcentre.com>
2025-08-25 14:29:19 +0545

    feat: yup and react-hook-form implementation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,18 @@
 import ParallaxScrollView from "@/components/ParallaxScrollView";
+import CustomInput from "@/components/reusable/CustomInput";
+import { AuthSchema } from "@/schema/auth-schema";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { Image } from "expo-image";
-import { Text, View } from "react-native";
-import "../../global.css";
+import { useForm } from "react-hook-form";
+import { Button, View } from "react-native";
+
 export default function HomeScreen() {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(AuthSchema) });
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
@@ -14,9 +24,25 @@ export default function HomeScreen() {
       }
     >
       <View className="items-center mt-4 mb-2">
-        <Text className="text-3xl font-bold text-center">
-          Welcome to Tailwind
-        </Text>
+        <CustomInput
+          control={control}
+          id="email"
+          label="Email"
+          error={errors}
+          placeholder="Enter your email"
+        />
+        <CustomInput
+          control={control}
+          id="password"
+          label="Password"
+          error={errors}
+          placeholder="Enter your password"
+          secureTextEntry
+        />
+        <Button
+          title="Submit"
+          onPress={handleSubmit((data) => console.log(data))}
+        />
       </View>
     </ParallaxScrollView>
   );
